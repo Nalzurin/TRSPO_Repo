810d3e625b475c0656f3f8c3b4e8b9b0d0373d6b
Nalzurin <alexnormandi@ukr.net>
2024-03-02 16:55:38 +0200

    Make random values a separate function for result repeatability

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -12,22 +12,27 @@ class MonteCarlo
         squareSize = 2*radius;
         Console.WriteLine("Input the number of points:");
         int totalPoints = int.Parse(Console.ReadLine());
-
+        double[] randomNumbers = new double[totalPoints*2];   
         Random random = new Random(12345);
+        for(int i = 0; i<totalPoints*2;i++)
+        {
+            randomNumbers[i] = random.NextDouble() * squareSize;
+        }
         int pointsInside = 0;
-
+        int index = 0;
         Stopwatch sw = Stopwatch.StartNew();
         for (int i = 0; i < totalPoints; i++)
         {
-            double x = random.NextDouble() * squareSize;
-            double y = random.NextDouble() * squareSize;
-            if(IsPointInside(x, y, radius))
+            double x = randomNumbers[index];
+            double y = randomNumbers[index + 1];
+            if (IsPointInside(x, y, radius))
             {
                 pointsInside++;
             }
+            index += 2;
         }
 
-        double approximateArea = (totalPoints - (double)pointsInside) / totalPoints * Math.Pow(squareSize,2); 
+        double approximateArea = (totalPoints - (double)pointsInside / totalPoints) * Math.Pow(squareSize,2); 
         sw.Stop();
 
         Console.WriteLine($"Approximate area: {approximateArea}");
