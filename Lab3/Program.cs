e62f51b50a5ed724eabd721dcd8c29c2e555aa2b
Nalzurin <alexnormandi@ukr.net>
2024-03-02 19:50:52 +0200

    Implement Client Server for asynchronicity

---
+++ Lab3/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the radius of circle:");
+        int radius = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the number of threads:");
+        int numThreads = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the number of points:");
+        int PointsNum = int.Parse(Console.ReadLine());
+        int iterationsPerThread = PointsNum / numThreads;
+        int remainder = PointsNum % numThreads;
+        int[] results = new int[numThreads];
+
+        double[] randomNumbers = new double[PointsNum * 2];
+        Random random = new Random(12345);
+        for (int i = 0; i < PointsNum * 2; i++)
+        {
+            randomNumbers[i] = random.NextDouble() * radius*2;
+        }
+
+
+        Stopwatch sw = Stopwatch.StartNew();
+        Thread[] threads = new Thread[numThreads];
+        int index = 0;
+        for (int i = 0; i < numThreads; i++)
+        {
+            int threadIndex = i;
+            threads[i] = new Thread(() =>
+            {
+                if (i == numThreads - 1)
+                {
+                    results[threadIndex] = MonteCarloSimulation(randomNumbers, index, iterationsPerThread + remainder, radius);
+                }
+                else
+                {
+                    results[threadIndex] = MonteCarloSimulation(randomNumbers, index, iterationsPerThread, radius);
+                }
+            });
+            index += iterationsPerThread;
+        }
+        
+        foreach (var thread in threads)
+        {
+            thread.Start();
+        }
+
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+
+        double totalInsideCircle = 0;
+        foreach (int result in results)
+        {
+            totalInsideCircle += result;
+        }
+        sw.Stop();
+
+        double estimatedArea = (PointsNum - totalInsideCircle) / PointsNum * Math.Pow(radius*2,2);
+
+        Console.WriteLine($"Estimated area of the circle: {estimatedArea}");
+        double exactArea = CalculateExactArea(radius);
+
+        Console.WriteLine($"Exact area: {exactArea}");
+        double error = Math.Abs((exactArea - estimatedArea) / exactArea) * 100;
+
+        Console.WriteLine($"Margin of error: {error} %");
+        Console.WriteLine($"Elapsed time: {sw.Elapsed}");
+    }
+
+    static int MonteCarloSimulation(double[] rand, int index, int iterations, double radius)
+    {
+        int insideCircle = 0;
+
+        for (int i = 0; i < iterations; i++)
+        {
+            double x = rand[index];
+            double y = rand[index+1];
+
+            // Check if the point (x, y) is inside the unit circle
+            if (x * x + y * y <= radius * radius)
+            {
+                insideCircle++;
+            }
+            index+=2;
+        }
+
+        return insideCircle;
+    }
+    static double CalculateExactArea(double radius)
+    {
+        return Math.PI * radius * radius;
+    }
+}
---
+++ Lab3_Client/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Text;
+
+class MonteCarloClient
+{
+    static void Main()
+    {
+        try
+        {
+            TcpClient client = new TcpClient("127.0.0.1", 8888);
+            NetworkStream stream = client.GetStream();
+
+            Console.WriteLine("Input circle radius:");
+            double radius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input the number of points:");
+            int totalPoints = int.Parse(Console.ReadLine());
+
+            double squareSize = 2 * radius;
+            double[] randomNumbers = new double[totalPoints * 2];
+            Random random = new Random(12345);
+
+            for (int i = 0; i < totalPoints * 2; i++)
+            {
+                randomNumbers[i] = random.NextDouble() * squareSize;
+            }
+
+            // Send radius and random numbers array to the server
+            Stopwatch sw = Stopwatch.StartNew();
+            double[] randomNumbersServer = new double[totalPoints/2];
+            Array.ConstrainedCopy(randomNumbers, totalPoints/2, randomNumbersServer,0, totalPoints/2);
+
+            string data = $"{radius};{string.Join(",", randomNumbersServer)}";
+            byte[] dataBytes = Encoding.ASCII.GetBytes(data);
+            stream.Write(dataBytes, 0, dataBytes.Length);
+
+            int pointsInsideClient = 0;
+            int index = 0;
+
+            for (int i = 0; i < totalPoints/2; i++)
+            {
+                double x = randomNumbers[index];
+                double y = randomNumbers[index + 1];
+                if (IsPointInside(x, y, radius))
+                {
+                    pointsInsideClient++;
+                }
+                index += 2;
+            }
+
+
+            // Receive result from the server
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string serverData = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            int serverPointsInside = int.Parse(serverData);
+            int pointsInside = serverPointsInside + pointsInsideClient;
+            double approximateArea = (totalPoints - (double)pointsInside) / totalPoints * Math.Pow(squareSize, 2);
+            sw.Stop();
+            Console.WriteLine($"Approximate area: {approximateArea}");
+            double exactArea = CalculateExactArea(radius);
+            Console.WriteLine($"Exact area: {exactArea}");
+            double error = Math.Abs((exactArea - approximateArea) / exactArea) * 100;
+            Console.WriteLine($"Margin of error: {error} %");
+            Console.WriteLine($"Elapsed time: {sw.Elapsed}");
+            sw.Stop();
+            stream.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception: {ex.Message}");
+        }
+    }
+    static bool IsPointInside(double x, double y, double radius)
+    {
+        return (x * x) + (y * y) <= radius * radius;
+    }
+    static double CalculateExactArea(double radius)
+    {
+        return Math.PI * radius * radius;
+    }
+}
